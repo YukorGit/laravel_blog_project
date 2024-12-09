8b78d98f25d33123c07a378b514a4957186ab6f8
Yuriy <yuriyseraz@gmail.com>
2024-12-10 00:43:23 +0300

    build

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,25 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
+
+    const ROLE_ADMIN = 0;
+    const ROLE_READER = 1;
+
+    public static function getRoles()
+    {
+        return [
+            self::ROLE_ADMIN =>'Админ',
+            self::ROLE_READER => 'Читатель',
+        ];
+    }
 
     /**
      * The attributes that are mass assignable.
